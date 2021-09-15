7f07e3873bb8ab1b775e8f0dbccf71fbb6d9ae14
FROISSART KEVIN p2002504 <p2002504@b710l1016.univ-lyon1.fr>
2021-09-15 19:01:45 +0200

    suite tp1

--- TP01/python/typecheck.py
+++ TP01/python/typecheck.py
@@ -2,7 +2,7 @@
 # name: type
 int_variable: int
 float_variable: float
-int_variable = 4.2  # Static typing error, but no runtime error
+int_variable = int(4.2)  # Static typing error, but no runtime error
 float_variable = 42.0  # OK
 float_variable = int_variable  # OK
 
@@ -13,5 +13,5 @@ def int_to_string(i: int) -> str:
     return str(i)
 
 
-print(int_to_string('Hello'))  # Static typing error, but no runtime error
-print(int_to_string(42) / 5)  # Both static and runtime error
+print(int_to_string(int('Hello')))  # Static typing error, but no runtime error
+print(int(int_to_string(42)) / 5)  # Both static and runtime error
--- TP01/riscv/minmax.s
+++ TP01/riscv/minmax.s
@@ -4,6 +4,7 @@ main:
 	addi	sp,sp,-16
 	sd	ra,8(sp)
 ## TODO Your assembly code there
+	
 ## END TODO - end of user assembly code
 	ld	ra,8(sp)
 	addi	sp,sp,16
