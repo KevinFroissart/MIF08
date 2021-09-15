4d8948a75fdb20fe21ef15cbc205383d8a8058e9
KevinFroissart <kevin.froissart69@gmail.com>
2021-09-15 23:06:24 +0200

    suite tp1

--- TP01/riscv/minmax.s
+++ TP01/riscv/minmax.s
@@ -4,8 +4,20 @@ main:
 	addi	sp,sp,-16
 	sd	ra,8(sp)
 ## TODO Your assembly code there
-	
+	li t0,5
+	li t1,10
+	bgt t0,t1,else
+	sd t1,min
+	call	print_int
+	call	newline
+	j end
+	else:
+		sd t0,min
+		call	print_int
+		call	newline
+		j end
 ## END TODO - end of user assembly code
+end:
 	ld	ra,8(sp)
 	addi	sp,sp,16
 	jr	ra
