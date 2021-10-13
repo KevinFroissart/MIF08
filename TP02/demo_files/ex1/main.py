c047623f67be6dd4055154155ad46cf00afd56e6
FROISSART KEVIN p2002504 <p2002504@b710l1118.univ-lyon1.fr>
2021-10-13 15:47:15 +0200

    pull moy

--- TP02/demo_files/ex1/main.py
+++ TP02/demo_files/ex1/main.py
@@ -1,6 +1,10 @@
 from antlr4 import InputStream
 from antlr4 import CommonTokenStream
+<<<<<<< HEAD
 from Exo3 import Example1
+=======
+from Example1 import Example1
+>>>>>>> 55664ee82249a0e32f6b0e6de45e1963b36eff11
 
 import sys
 
@@ -9,7 +13,11 @@ def main():
     # InputStream reads characters (from stdin in our case)
     input_stream = InputStream(sys.stdin.read())
     # The generated lexer groups characters into Tokens ...
+<<<<<<< HEAD
     lexer = Exo3(input_stream)
+=======
+    lexer = Example1(input_stream)
+>>>>>>> 55664ee82249a0e32f6b0e6de45e1963b36eff11
     # ... and the stream of Tokens is managed by the TokenStream.
     stream = CommonTokenStream(lexer)
 
